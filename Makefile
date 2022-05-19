d2cbf81bdf7db65344b73bbff8d5a4b3338ef755
Tudor <tuddor144@gmail.com>
2022-05-19 08:53:01 -0700

    Added Tests

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ install:
 	npm install --legacy-peer-deps
 run:
 	npm start
+
+test:
+	npm test -- --watchAll=false
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const someVar = 0
+  expect(someVar).toEqual(0);
 });
